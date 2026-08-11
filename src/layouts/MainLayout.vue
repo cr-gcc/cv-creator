3c500d6e47b1f7aaf86086677c4f0a72f41998f6
Cris GC <cris.gcc@outlook.com>
2026-08-11 17:34:22 -0600

    Side bar menu acoplado

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+    import Sidebar from '@layouts/Sidebar.vue'
 </script>
 
 <template>
     <div id="main-layout">
         <div class="min-h-screen flex flex-col">
+            <Sidebar />
             <main class="flex-1">
                 <slot />
             </main>

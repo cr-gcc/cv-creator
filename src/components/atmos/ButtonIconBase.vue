9bd9a2815ddb9cfea987d4dca9f2e50c1581387f
Cris GC <cris.gcc@outlook.com>
2026-08-11 20:09:52 -0600

    Creación del componente ButtonIcon

---
+++ src/components/atmos/ButtonIconBase.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    withDefaults(defineProps<{
+        color?: string;
+        iconSize?: string;
+        icon?: string;
+    }>(), {
+        color: 'text-primary',
+        iconSize: 'text-md',
+        icon: '',
+    })
+
+    const emit = defineEmits(['button-clicked'])
+
+    const handleClick = () => {
+        emit('button-clicked')
+    }
+</script>
+
+<template>
+    <button class="flex items-center cursor-pointer" @click="handleClick()">
+        <i :class="[color, iconSize, icon]"></i>
+    </button>
+</template>

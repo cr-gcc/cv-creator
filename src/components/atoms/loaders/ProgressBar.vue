5b766f24003f1fb83eb90df3edb3c946abeb2cee
Cris GC <cris.gcc@outlook.com>
2026-08-13 15:23:32 -0600

    Creación de ProgressBar

---
+++ src/components/atoms/loaders/ProgressBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="w-full bg-border rounded-full h-3 overflow-hidden">
+    <div
+      class="h-3 bg-primary animate-progress rounded-full"
+      :style="`width: 50%; animation-duration: 1.2s`"
+    ></div>
+  </div>
+</template>
+<style>
+  @keyframes progress {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(100%);
+    }
+  }
+  .animate-progress {
+    animation-name: progress;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+</style>
